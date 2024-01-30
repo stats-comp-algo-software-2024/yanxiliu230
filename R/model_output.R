4480144bb88c485af37f0839088594054143a5e0
Nora Liu <yanxiliu23@gmail.com>
2024-01-30 16:27:57 -0500

    add model output functions coef, vcov and print

---
+++ R/model_output.R
@@ -0,0 +1,17 @@
+#' @export
+coef.hglm <- function(hglm_out) {
+  # TODO: actually compute the coefficients
+  print("coefficients")
+}
+
+#' @export
+vcov.hglm <- function(hglm_out) {
+  # TODO: actually compute the CI
+  print("confidence interval")
+}
+
+#' @export
+print.hglm <- function(hglm_out) {
+  # TODO: print something useful
+  print("print output")
+}
